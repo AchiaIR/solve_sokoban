fc6fa84dd817c3515a41ab041cffbefd321eaf36
Achia Rosin <Achia.Rosin@ceva-dsp.com>
2023-08-19 03:45:49 +0300

    changes to run files

--- run.bat
+++ run.bat
@@ -1,1 +1,6 @@
-python solve_sokoban.py -config_file config.yaml
+@echo off
+IF "%~1"=="" (
+    python solve_sokoban.py -config_file config.yaml
+) ELSE (
+    python solve_sokoban.py -config_file config.yaml SOKOBAN.NUM_BOXES %1 
+)
--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-python solve_sokoban.py -config_file config.yaml
+#!/bin/bash
+
+if [ "$#" -eq 0 ]; then
+    python solve_sokoban.py -config_file config.yaml
+else
+    python solve_sokoban.py -config_file config.yaml SOKOBAN.NUM_BOXES $1 
+fi
